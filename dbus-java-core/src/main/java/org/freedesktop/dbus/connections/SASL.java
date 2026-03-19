d3a198094a88062a551681d80865bb104424a6d0
Evgeny Basargin <19927545+eugenebas@users.noreply.github.com>
2026-03-20 00:51:13 +0300

    again9

--- dbus-java-core/src/main/java/org/freedesktop/dbus/connections/SASL.java
+++ dbus-java-core/src/main/java/org/freedesktop/dbus/connections/SASL.java
@@ -62,7 +62,7 @@ public class SASL {
     static {
         try {
             COL.setDecomposition(2);
-        } catch(Exception e) { // Ignore the error, this is not critical
+        } catch (Exception setDecompositionException) { // Ignore the error, this is not critical
         }
         COL.setStrength(Collator.PRIMARY);
     }
