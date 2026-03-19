9be8b67bd59877f48caa283c21d6bd5ea6da17a4
Evgeny Basargin <19927545+eugenebas@users.noreply.github.com>
2026-03-20 00:22:27 +0300

    again5

--- dbus-java-transport-tcp/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.freedesktop.dbus.transport.tcp {
-
-    requires org.freedesktop.dbus;
-
-    provides org.freedesktop.dbus.spi.transport.ITransportProvider
-            with
-            org.freedesktop.dbus.transport.tcp.TcpTransportProvider;
-}
